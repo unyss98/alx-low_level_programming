3a9c169c1b8938ee4caec3802386896352a186c9
unyss98 <eunicedunyo@gmail.com>
2023-03-16 22:50:13 -0700

    allocate sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	float f;
+	char c;
+	long int d;
+	long long int b;
+
+	printf("The size of an int is: %lu.\n" (unsigned long)sizeof(i));
+	printf("The size of a float is: %lu.\n" (unsigned long)sizeof(f);
+	printf("The size of a char is: %lu.\n" (unsigned long)sizeof(c);
+	printf("The size of a long int is: %lu.\n" (unsigned long)sizeof(d);
+	printf("The size of a long long int is: %lu.\n" (unsigned long)sizeof(b);
+
+	return (0);
+}
