c18954ec8d0b4caa453993d368f7e3af20769abc
unyss98 <eunicedunyo@gmail.com>
2023-03-19 10:57:02 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 0x0; c <= 0xF; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
