47eaba969ae37b61484ade78e18070fb4bec1b5d
unyss98 <eunicedunyo@gmail.com>
2023-03-19 16:10:23 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,14 +11,13 @@ int main(void)
 	char c;
 	int i;
 
-	i >= 0x10;
-
 	for (c = 0x0; c <= 0xf; c++)
 		if (c <= 0x9)
 	{
 		putchar('0' + c);
 	}
 		else
+			i >= 0x10;
 			while (i <= 0xf)
 		{
 			putchar('i');
