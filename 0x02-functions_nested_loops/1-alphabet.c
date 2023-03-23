ed1046ddecaaa74301862e176029cf148e90b49d
unyss98 <eunicedunyo@gmail.com>
2023-03-23 06:31:55 -0700

    update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Description - 'prints alphabet'
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	{
+	_putchar(alpha);
+	_putchar('\n');
+	}
+}
