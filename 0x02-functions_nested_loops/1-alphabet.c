f543ad9d272afc36599f60db45c7fcf764ed3afd
unyss98 <eunicedunyo@gmail.com>
2023-03-23 06:38:11 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,9 +10,10 @@ void print_alphabet(void)
 {
 	char alpha;
 
-	for (alpha ='a'; alpha <= 'z'; alpha++)
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 	_putchar(alpha);
-	_putchar('\n');
 	}
+
+	_putchar('\n');
 }
