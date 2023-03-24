e93b58320ba8febaab251c371f48bbc88d15706f
unyss98 <eunicedunyo@gmail.com>
2023-03-23 21:08:22 -0700

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ * Description - 'prints alphabet ten times'
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i;
+
+	i = 0;
+	while (i > 10)
+	{
+		alpha = 'a';
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+}
