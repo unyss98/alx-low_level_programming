1ea672541c88935a4f6177823736a4ce1d31a3fa
unyss98 <eunicedunyo@gmail.com>
2023-03-24 16:17:22 -0700

    print absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - 'computes absolute value of integer'
+ *
+ * @i: an unsigned integer
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int i)
+{
+	_abs(i);
+	return (0);
+
+	_putchar('\n');
+}
