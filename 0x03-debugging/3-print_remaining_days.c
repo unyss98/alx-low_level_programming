e04f3fab266e353ba6cbbad7a0111a0bee38367b
unyss98 <eunicedunyo@gmail.com>
2023-03-23 17:56:23 -0700

    print date

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -31,6 +31,11 @@ void print_remaining_days(int month, int day, int year)
 		}
 		else
 		{
+			if ((year % 4 != 0) && (day > 0))
+			{
+				day++;
+			}
+
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
