d4f18abcf0fee8904e153eab0180a99b1120f196
unyss98 <eunicedunyo@gmail.com>
2023-03-25 12:15:19 -0700

    print 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,31 +15,25 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			k = i * j;
-			if (k >= 0)
+			if (k > 9)
 			{
-				if (k < 10)
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + k / 10);
+				_putchar('0' + k % 10);
+			}
+			else
+			{
+				if (j != 0)
 				{
-					_putchar('0' + k);
-					_putchar(',');
-					if (k++ >= 10)
-					{
-						_putchar(' ');
-					}
-					else
-					{
-						_putchar(' ');
-						_putchar(' ');
-					}
-				}
-				else
-				{
-					_putchar('0' + k / 10);
-					_putchar('0' + k % 10);
 					_putchar(',');
 					_putchar(' ');
+					_putchar(' ');
 				}
+				_putchar('0' + k);
 			}
 		}
 		_putchar('\n');
 	}
 }
+
