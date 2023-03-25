c68cd73fb1a2ff8ffe220031b39e04b762cb0268
unyss98 <eunicedunyo@gmail.com>
2023-03-24 18:34:17 -0700

    print minutes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -21,13 +21,13 @@ void jack_bauer(void)
 				for (d = 0; d <= 9; d++)
 				{
 					if (a >= 2 && b >= 4)
-					break;
-				_putchar(a + '0');
-				_putchar(b + '0');
-				_putchar(58);
-				_putchar(c + '0');
-				_putchar(d + '0');
-				_putchar('\n');
+						break;
+					_putchar(a + '0');
+					_putchar(b + '0');
+					_putchar(58);
+					_putchar(c + '0');
+					_putchar(d + '0');
+					_putchar('\n');
 				}
 			}
 		}
