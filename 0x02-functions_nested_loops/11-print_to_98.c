baf6cd82ad1b5a69c36c63fff8ce5e4a8d94e9f6
unyss98 <eunicedunyo@gmail.com>
2023-03-24 20:26:41 -0700

    print natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints natural numbers
+ *
+ * @n: input number
+ */
+
+void print_to_98(int n)
+{
+	for (n = 1; n <= 98; n++)
+	{
+		_putchar('n');
+		_putchar(',');
+		_putchar(' ');
+	}
+}
