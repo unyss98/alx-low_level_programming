e7c86ee56eabf618a7db4493013c13b249563fe5
unyss98 <eunicedunyo@gmail.com>
2023-03-24 19:37:35 -0700

    s

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,12 +8,14 @@ void times_table(void)
 {
 	int i;
 	int j;
+	int k;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			_putchar('0' + i * j);
+			k = i * j;
+			_putchar('0' + k);
 			_putchar(',');
 		}
 		_putchar('\n');
