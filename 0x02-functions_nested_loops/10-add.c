3f20c391cf86448e9e28ef49163ff7fac8053112
unyss98 <eunicedunyo@gmail.com>
2023-03-24 20:10:19 -0700

    add integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds integers
+ *
+ * @a: input number
+ * @b: input number
+ *
+ * Return: value
+ */
+
+int add(int a, int b)
+{
+	int num;
+
+	num = a + b;
+	return ('0' + num);
+}
