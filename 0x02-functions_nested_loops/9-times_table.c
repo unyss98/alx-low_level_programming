775371493db20136363368bd47bd0a92bded5b09
unyss98 <eunicedunyo@gmail.com>
2023-03-24 18:58:40 -0700

    print 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * times_table - prints times table
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar(i * j);
+			_putchar(',');
+		}
+		_putchar('\n');
+	}
+}
