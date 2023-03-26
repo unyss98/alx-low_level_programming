24fcf1fce19f427c0e916a135b2410d07815f8f6
unyss98 <eunicedunyo@gmail.com>
2023-03-25 17:13:45 -0700

    print some numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		if (a != 2 && a != 4)
+		{
+			_putchar('0' + a);
+		}
+	}
+	_putchar('\n');
+}
