e3743a132691448a2aa992216c625b1576fa6431
unyss98 <eunicedunyo@gmail.com>
2023-03-25 17:03:55 -0700

    print digit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers to output
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar('0' + a);
+	}
+	_putchar('\n');
+}
