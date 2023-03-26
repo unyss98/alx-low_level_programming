bc26d87e6f1a5c71383e73f3b9eca143068e6e6d
unyss98 <eunicedunyo@gmail.com>
2023-03-25 19:24:32 -0700

    print diagoonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a line
+ *
+ * @n: input number
+ */
+
+void print_diagonal(int n)
+{
+	int m;
+	int q;
+
+	if (n > 0)
+	{
+		for (m = 1; m <= n; m++)
+		{
+			for (q = 1; q <= n; q++)
+			{
+				if (m == q)
+				{
+					_putchar(92);
+				}
+				else
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
