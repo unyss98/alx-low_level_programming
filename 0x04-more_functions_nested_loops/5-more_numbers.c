cc4f505027ee7531fe90e121e11a74643e28c8d1
unyss98 <eunicedunyo@gmail.com>
2023-03-25 17:50:54 -0700

    print more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,25 +8,16 @@ void more_numbers(void)
 {
 	int i;
 	int j;
-	int k;
-	int m;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j <= 9)
+			if (j > 9)
 			{
-				_putchar('0' + j);
+				_putchar('0' + j / 10);
 			}
-			else
-			{
-				k = j / 10;
-				m = j % 10;
-
-				_putchar('0' + k);
-				_putchar('0' + m);
-			}
+			_putchar('0' + j % 10);
 		}
 		_putchar('\n');
 	}
