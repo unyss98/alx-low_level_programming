1f6e8938f599f68a71f502ea0193e5749c3b6071
unyss98 <eunicedunyo@gmail.com>
2023-03-25 22:44:17 -0700

    create buzz and fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,18 +11,15 @@ int main(void)
 	{
 		if ((a % 3 == 0) && (a % 5 == 0))
 		{
-			printf("FizzBuzz");
-			printf(" ");
+			printf("FizzBuzz ");
 		}
 		else if (a % 3 == 0)
 		{
 			printf("Fizz ");
-			printf(" ");
 		}
 		else if (a % 5 == 0)
 		{
 			printf("Buzz ");
-			printf(" ");
 		}
 		else
 		{
