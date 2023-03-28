10fa85c3b673ce7613b216d0e3a7f6de89952050
unyss98 <eunicedunyo@gmail.com>
2023-03-27 21:04:27 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+ * _puts - prints a string to stdout
+ *
+ * @str - a string
+ *
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		_putchar('0' + i);
+		_putchar('\n');
+	}
+}
