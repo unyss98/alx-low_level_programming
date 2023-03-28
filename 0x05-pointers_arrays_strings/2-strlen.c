eaf4b8366efc1ba5ff03813c3cb751843047ef5e
unyss98 <eunicedunyo@gmail.com>
2023-03-27 20:42:51 -0700

    print length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - print length of a string
+ *
+ * @s: a string
+ *
+ * Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		_putchar('0' + i);
+	}
+	return (i);
+}
