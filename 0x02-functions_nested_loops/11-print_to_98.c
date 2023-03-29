5e12ea44f929ad2ac69a4d14779ecf60b4db0530
unyss98 <eunicedunyo@gmail.com>
2023-03-29 07:58:14 -0700

    print natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,55 +1,37 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - prints natural numbers
+ * @n: input number
  *
  */
 
 void print_to_98(int n)
 {
 	int k;
-	int l;
-	
-	if (n == 98);
-	{
-		_putchar(0 + 'n');
-		_putchar('\n')
-	}
+
+	if (n > 98)
+		for (k = n; k >= 98; k--)
+		{
+			printf("%d", k);
+			if (k != 98)
+			{
+				printf(", ");
+			}
+		}
+
+	else if (n < 98)
+		for (k = n; k <= 98; k++)
+		{
+			printf("%d", k);
+			if (k != 98)
+			{
+				printf(", ");
+			}
+		}
 	else
 	{
-		if (n < 98)
-		{
-			for (k = n; k <= 98; k++)
-			{
-				if (k == n)
-				{
-					_putchar('0' + k);
-				}
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar('0' + k);
-				}
-			}
-			_putchar('\n');
-		}
-		if (n > 98)
-		{
-			for (l = n; l >= 98; l--)
-			{
-				if (l == n)
-				{
-					_putchar('0' + l);
-				}
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar('0' + k);
-				}
-			}
-			_putchar('\n');
-		}
+		printf("%d", n);
 	}
+	putchar('\n');
 }
