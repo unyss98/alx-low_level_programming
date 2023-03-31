57749095403a9f9e118d79d4b9b3752f1a5a680f
unyss98 <eunicedunyo@gmail.com>
2023-03-30 19:09:26 -0700

    print lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *string_toupper - prints lowercase to uppercase
+ *
+ * @s: pointer to string
+ *
+ * Return: string in upper case
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+	char s;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] = s[i] - 32;
+		}
+		i++;
+	}
+	return (s)
