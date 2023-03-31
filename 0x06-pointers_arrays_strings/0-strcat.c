63048fb1e826ada47a1ee8f167e2adbabff664c3
unyss98 <eunicedunyo@gmail.com>
2023-03-30 17:51:57 -0700

    concatenate strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,13 +14,15 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int j;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		_putchar(dest[i]);
-		for (j = 0; src[j] <= '\0'; j++)
-		{
-			_putchar(src[j]);
-		}
+		i++;
 	}
+	for (j = 0; src[j] != '\0'; j++, i++)
+	{
+		dest[i] = src[j];
+	}
+	dest[i] = '\0';
 	return (dest);
 }
