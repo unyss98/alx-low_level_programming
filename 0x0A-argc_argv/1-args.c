175842ac8a8a3e71c9e9ea53909d0abc012e4755
unyss98 <eunicedunyo@gmail.com>
2023-04-09 17:06:38 -0700

    print number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: number of parameters to be passed
+ * @argv: parameter to be passed
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
