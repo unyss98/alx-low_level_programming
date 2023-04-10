45bcb221b50ab6ed0fddedd52e8d4077c489581b
unyss98 <eunicedunyo@gmail.com>
2023-04-09 20:56:12 -0700

    add arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: number of parameters
+ * @argv: parameter
+ *
+ * Return: 0 if success, 1 if not
+ */
+
+int main(int argc, char *argv[])
+{
+	int a;
+	int sum;
+	int c;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (a = 1; a < argc; a++)
+		{
+			c = atoi(argv[a]);
+			if (c == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				sum = 0;
+				sum += c;
+			}
+		}
+		printf("%d\n", sum += c);
+	}
+	return (0);
+}
