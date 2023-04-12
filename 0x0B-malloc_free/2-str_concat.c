6ecbe1bd978a5f6ea2632fab9ecdca94170e2d30
unyss98 <eunicedunyo@gmail.com>
2023-04-12 08:50:14 -0700

    concatenate strings to new location

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,10 +2,8 @@
 #include <stdlib.h>
 /**
  * str_concat - returns pointer to concatenated string
- *
  * @s1: pointer to a string
  * @s2: pointer to a string
- *
  * Return: pointer to concatenated string
  */
 char *str_concat(char *s1, char *s2)
@@ -16,34 +14,35 @@ char *str_concat(char *s1, char *s2)
 	int i;
 	int j;
 
-	if (s1 != NULL)
+	s1len = 0;
+	while (s1[s1len] != '\0')
 	{
-		s1len = 0;
-		while (s1[s1len] != '\0')
-		{
-			s1len++;
-		}
+		s1len++;
 	}
-	if (s2 != NULL)
+	s2len = 0;
+	while (s2[s2len] != '\0')
 	{
-		s2len = 0;
-		while (s2[s2len] != '\0')
-		{
-			s2len++;
-		}
+		s2len++;
 	}
 	p = malloc((s1len + s2len) * sizeof(char) + 1);
 	if (p == NULL)
+	{
 		return (NULL);
-	if (s1 != NULL)
+	}
+	else
 	{
 		for (i = 0; i < s1len; i++)
+		{
+			if (s1 == NULL)
+				break;
 			p[i] = s1[i];
-	}
-	if (s2 != NULL)
-	{
+		}
 		for (j = 0; j < s2len; j++)
+		{
+			if (s2 == NULL)
+				break;
 			p[i + j] = s2[j];
+		}
 	}
 	p[i + j] = '\0';
 	return (p);
