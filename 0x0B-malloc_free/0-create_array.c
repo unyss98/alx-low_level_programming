aa2901f80ab427ee15979941752dc7f1e16dd0aa
unyss98 <eunicedunyo@gmail.com>
2023-04-11 19:46:38 -0700

    create and initialise array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - create an array of characters
+ *
+ * @size: an unsigned integer
+ *
+ * @c: a pointer to character c
+ *
+ * Return: pointer to C or NULL
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	p = malloc(size * sizeof(char));
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
+	return (p);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+tasks on dynamic memory allocation
