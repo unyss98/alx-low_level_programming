46e3da8a33ba3b12540e26e32ea046298d169573
unyss98 <eunicedunyo@gmail.com>
2023-04-11 21:21:08 -0700

    copy string to new memory location

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+ * _strdup - returns a pointer which contains a copy of a string
+ *
+ * @str: pointer to a string
+ *
+ * Return: a pointer to a copy of str
+ */
+
+char *_strdup(char *str)
+{
+	char *p;
+	int len;
+	int i;
+
+	if (*str == '\0')
+	{
+		return (NULL);
+	}
+	else
+	{
+		len = 0;
+		while (str[len] != '\0')
+		{
+			len++;
+		}
+
+		p = malloc(len * sizeof(char) + 1);
+		if (p == NULL)
+		{
+			return (NULL);
+		}
+
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			p[i] = str[i];
+		}
+		p[i] = '\0';
+	}
+	return (p);
+}
