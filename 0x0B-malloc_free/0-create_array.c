4a6b97667ef42abc21baed90912a1f405c72e3cd
unyss98 <eunicedunyo@gmail.com>
2023-04-11 20:04:03 -0700

    create and initialise array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -22,7 +22,7 @@ char *create_array(unsigned int size, char c)
 	}
 	else
 	{
-		p = malloc(size * sizeof(char));
+		p = (char *) malloc(size * sizeof(char));
 		for (i = 0; i < size; i++)
 		{
 			p[i] = c;
