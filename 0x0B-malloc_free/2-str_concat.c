05de7542dcdf61a87602a00e74ccf9bb29b060fc
unyss98 <eunicedunyo@gmail.com>
2023-04-12 21:47:18 -0700

    concatenate strings to new memory location

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,25 +9,34 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *p;
-	int s1len;
-	int s2len;
-	int i;
-	int j;
+	int s1len, s2len, i, j;
 
 	s1len = 0;
 	while (s1len != '\0')
 	{
 		s1len++;
 	}
+	if (s1 == NULL)
+	{
+		s1len = 0;
+	}
 	s2len = 0;
 	while (s2len != '\0')
 	{
 		s2len++;
 	}
+	if (s2 == NULL)
+	{
+		s2len = 0;
+	}
 	p = malloc((s1len + s2len + 1) * sizeof(char));
 	if (p == NULL)
 	{
 		return (NULL);
+	}
+	if (s1 == NULL && s2 == NULL)
+	{
+		p = '\0';
 	}
 	for (i = 0; s1[i] != '\0'; i++)
 	{
