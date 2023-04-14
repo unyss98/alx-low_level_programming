7b927c5f1a7fce520447aba7cc8cd5f47b97ed03
unyss98 <eunicedunyo@gmail.com>
2023-04-13 20:20:43 -0700

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,43 +9,30 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *p;
-	int s1len, s2len, i, j;
+	int s1len = 0, s2len = 0, i, j;
 
-	s1len = 0;
-	while (s1len != '\0')
-	{
-		s1len++;
-	}
 	if (s1 == NULL)
-	{
-		s1len = 0;
-	}
-	s2len = 0;
-	while (s2len != '\0')
-	{
-		s2len++;
-	}
+		s1 = " ";
 	if (s2 == NULL)
-	{
-		s2len = 0;
-	}
-	p = malloc((s1len + s2len) * sizeof(char) + 1);
+		s2 = " ";
+
+	while (s1[s1len] != '\0')
+		s1len++;
+
+	while (s2[s2len] != '\0')
+		s2len++;
+
+	p = malloc((s1len + s2len + 1) * sizeof(char));
 	if (p == NULL)
-	{
 		return (NULL);
-	}
-	if (s1 == NULL && s2 == NULL)
-	{
-		p = '\0';
-	}
+
 	for (i = 0; s1[i] != '\0'; i++)
-	{
 		p[i] = s1[i];
-	}
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-		p[i + j] = s2[j];
-	}
-	p[i + j] = '\0';
+
+	for (j = 0; s2[j] != '\0'; j++, i++)
+		p[i] = s2[j];
+
+	p[i] = '\0';
+
 	return (p);
 }
