08f31b6de71049642d2832f74fd5feaf8f4d334e
unyss98 <eunicedunyo@gmail.com>
2023-04-13 21:59:34 -0700

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -26,11 +26,14 @@ char *str_concat(char *s1, char *s2)
 	if (p == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i] != '\0'; i++)
-		p[i] = s1[i];
-
-	for (j = 0; s2[j] != '\0'; j++, i++)
-		p[i] = s2[j];
+	if (s1 != NULL && s2 != NULL)
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+			p[i] = s1[i];
+		
+		for (j = 0; s2[j] != '\0'; j++, i++)
+			p[i] = s2[j];
+	}
 
 	p[i] = '\0';
 
