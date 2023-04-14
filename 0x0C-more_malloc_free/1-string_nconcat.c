3e0a475a0ecbf0550ec28615e863c1b73f095ddc
unyss98 <eunicedunyo@gmail.com>
2023-04-13 19:51:14 -0700

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -43,7 +43,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		{
 			p[i] = s1[i];
 		}
-		for (j = 0; j <= n && s2[j] != '\0'; j++, i++)
+		for (j = 0; j < n && s2[j] != '\0'; j++, i++)
 		{
 			p[i] = s2[j];
 		}
