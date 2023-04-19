1dfd26099c00ee35f4f142582a505dddda948cad
unyss98 <eunicedunyo@gmail.com>
2023-04-19 13:48:29 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ *
+ * @name: a pointer to string
+ * @f: a pointer to function
+ *
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+tasks on function pointers
