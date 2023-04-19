10de05853f228ce3522fddd20f178f962985326a
unyss98 <eunicedunyo@gmail.com>
2023-04-19 14:30:14 -0700

    execute function passed as argument on elemnts of array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - execute function passed as argument onelements of array
+ *
+ * @array: pointer to an array
+ * @size: size of array
+ * @action: pointer to function to be passed as argument
+ *
+ * Return: nothing
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
