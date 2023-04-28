496d565c9d4ca29b394c628a122ac68b3dc759e5
unyss98 <eunicedunyo@gmail.com>
2023-04-27 22:51:08 -0700

    debugging

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -16,17 +16,13 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
 	list_t *temp;
-	unsigned int len = 0;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 
-	while (str[len])
-		len++;
-
 	new->str = strdup(str);
-	new->len = len;
+	new->len = strlen(new->str);
 	new->next = NULL;
 
 	if (*head == NULL)
