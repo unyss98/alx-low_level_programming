06ed3bbad5f1696248b0a476948c0f894470b311
unyss98 <eunicedunyo@gmail.com>
2023-04-28 18:22:21 -0700

    handle allocation fails

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -20,14 +20,14 @@ list_t *add_node_end(list_t **head, const char *str)
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 	{
-		free (new);
+		free(new);
 		return (NULL);
 	}
 
 	new->str = strdup(str);
 	if (new->str == NULL)
 	{
-		free (new);
+		free(new);
 		return (NULL);
 	}
 	new->len = strlen(new->str);
