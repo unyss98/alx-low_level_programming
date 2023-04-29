4672e37c8dd5b4dfb29b3bd6f2a10a77c85ed450
unyss98 <eunicedunyo@gmail.com>
2023-04-28 18:10:45 -0700

    debugging return success

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -32,7 +32,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		temp = *head;
 		while (temp->next != NULL)
 		temp = temp->next;
+		temp->next = new;
 	}
-	temp->next = new;
 	return (new);
 }
