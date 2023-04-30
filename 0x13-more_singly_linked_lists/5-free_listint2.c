faa36a87289b28b08e07707ac2b075f7e1cee350
unyss98 <eunicedunyo@gmail.com>
2023-04-30 16:37:49 -0700

    free list and set head to null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,10 +13,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current;
 
-	current = *head;
-	while (current != NULL)
+	while (*head != NULL)
 	{
-		current = current->next;
+		current = *head;
+		*head = (*head)->next;
 		free(current);
 	}
 	*head = NULL;
