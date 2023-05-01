3f34b5603aa1fbb72df50a74f520933426ae1b37
unyss98 <eunicedunyo@gmail.com>
2023-04-30 21:15:54 -0700

    debugging

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,15 +18,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 
 	if (*head == NULL)
-	{
-		exit(1);
-	}
+		return (NULL);
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
-	{
 		return (NULL);
-	}
+
 	new->n = n;
 
 	if (idx == 0)
@@ -40,15 +37,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		temp = temp->next;
 	}
-	if (i < idx - 1 || temp ==  NULL)
+	if (i < idx - 1 || temp == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
-	new->next = temp->next;
+	new->next  = temp->next;
 	temp->next = new;
+
 	return (new);
 }
-
-
-
