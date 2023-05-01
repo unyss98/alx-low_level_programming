c1b431cfd4a382d9969e1a4229e64bfd857bf2c7
unyss98 <eunicedunyo@gmail.com>
2023-04-30 21:28:33 -0700

    debugging

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new->next = NULL;
 		*head = new;
-		return(new);
+		return (new);
 	}
 	if (*head != NULL && idx == 0)
 	{
