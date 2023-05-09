e026f6db8b3992144bf10fb0ba3d328b8d30923f
unyss98 <eunicedunyo@gmail.com>
2023-05-08 21:15:22 -0700

    spend 4 hours read and printing to stdout only for your gihub account to waste 30 more mins

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -40,7 +40,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 	buffer[r] = '\0';
 	c = fwrite(buffer, sizeof(char), r, stdout);
-	if (c != r)
+	if (c < r)
 	{
 		free(buffer);
 		fclose(fp);
