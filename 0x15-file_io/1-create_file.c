7966f7ac7950698a14e74ec97da33854dcb4f4b7
unyss98 <eunicedunyo@gmail.com>
2023-05-10 17:27:26 -0700

    debugging

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,16 +23,8 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC | O_EXCL, 0600);
 	if (fd < 0)
-	{
-		if (errno == EEXIST)
-		{
-			fd = open(filename, O_WRONLY | O_TRUNC);
-			if (fd < 0)
-				return (-1);
-			close(fd);
-		}
 		return (-1);
-	}
+
 	if (text_content != NULL)
 	{
 		i = write(fd, text_content, strlen(text_content));
