9d2122d0e6fe190036265236f2b387b1fb61c2e2
unyss98 <eunicedunyo@gmail.com>
2023-05-12 19:55:31 -0700

    copy file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -40,10 +40,10 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fdfrom);
 		exit(100);
 	}
-	fdto = open(argv[2], O_WRONLY | O_CREAT, 0664 | O_EXCL);
+	fdto = open(argv[2], O_WRONLY | O_TRUNC);
 	if (fdto < 0)
 	{
-		fdto = open(argv[2], O_WRONLY | O_TRUNC);
+		fdto = open(argv[2], O_WRONLY | O_CREAT | O_EXCL, 0644);
 		if (fdto < 0)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
